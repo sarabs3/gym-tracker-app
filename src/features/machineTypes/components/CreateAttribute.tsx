4768ce2837be362cb879c24bb6d970a1ade38205
sarabs3js <46516570+sarabs3js@users.noreply.github.com>
2022-08-21 15:13:03 +0530

    Create attribute component finished

--- src/features/machineTypes/components/CreateAttribute.tsx
+++ src/features/machineTypes/components/CreateAttribute.tsx
@@ -7,15 +7,26 @@ const options = [
     FieldTypes.date,
     FieldTypes.checkbox
 ];
-const CreateAttribute = () => {
-    const [fieldTypeValue, setFieldTypeValue] = useState<FieldTypes>(options[0]);
+interface Props {
+    onNameChange: (e: string, v: string) => void
+    name: string;
+    type: FieldTypes;
+}
+const CreateAttribute: FC<Props> = ({ onNameChange, name, type }) => {
+    const [inputFeild, setInputField] = useState(name);
+    const [fieldTypeValue, setFieldTypeValue] = useState<FieldTypes>(type || options[0]);
     const selectFieldType = (e: ChangeEvent<HTMLSelectElement>) => {
-        setFieldTypeValue(e.target.value as FieldTypes)
+        setFieldTypeValue(e.target.value as FieldTypes);
+        onNameChange(e.target.name, e.target.value);
     };
+    const updateName = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputField(e.target.value);
+        onNameChange(e.target.name, e.target.value);
+    }
     return (
-    <div className='mb-2 flex '>
-        <input className='border' type="text" />
-        <select value={fieldTypeValue} onChange={selectFieldType}>
+    <div className='mb-2 flex'>
+        <input className='border' type="text" name='fieldName' value={inputFeild} onChange={updateName} />
+        <select name='fieldType' value={fieldTypeValue} onChange={selectFieldType}>
             {options.map(option => <option key={option} value={option}>{option}</option>) }
         </select>
     </div>
